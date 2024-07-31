1be560081bf8e3629d01bb5927038badb3a28419
mirageoasis <kimhw0820@naver.com>
2024-07-31 22:16:41 +0900

    7:31 -> new
    Signed-off-by: mirageoasis <kimhw0820@naver.com>

--- boj/1967.py
+++ boj/1967.py
@@ -11,30 +11,71 @@ N=int(input())
 ans=0
 # 부모 작은 순서대로
 
+# for i in range(N-1):
+#     a, b, c = map(int, sys.stdin.readline().split())
+#     chart[a].append([b, c])
+
+# def dfs(now):
+#     global ans
+#     #print(now)
+#     if len(chart[now]) == 0:
+#         return 0
+#     if len(chart[now]) == 1:
+#         ret = dfs(chart[now][0][0]) + chart[now][0][1]
+#         ans=max(ans, ret)
+#         return ret
+    
+#     # 노드 최대 값 + 자신과의 거리
+#     li = []
+#     for node, value in chart[now]:
+#         li.append(dfs(node) + value)
+    
+#     li.sort(reverse=True)
+#     ans=max(ans, li[0]+li[1])
+
+#     return li[0]
+
 for i in range(N-1):
     a, b, c = map(int, sys.stdin.readline().split())
     chart[a].append([b, c])
+    chart[b].append([a, c])
 
-def dfs(now):
-    global ans
-    #print(now)
-    if len(chart[now]) == 0:
-        return 0
-    if len(chart[now]) == 1:
-        ret = dfs(chart[now][0][0]) + chart[now][0][1]
-        ans=max(ans, ret)
-        return ret
-    
-    # 노드 최대 값 + 자신과의 거리
-    li = []
-    for node, value in chart[now]:
-        li.append(dfs(node) + value)
-    
-    li.sort(reverse=True)
-    ans=max(ans, li[0]+li[1])
+distance1=[0 for i in range(N+1)]
+visit=[False for i in range(N+1)]
 
-    return li[0]
+def dfs2(now, prev_dist):
+    for i, dist in chart[now]:
+        if not visit[i]:
+            distance1[i]=prev_dist+dist
+            visit[i]=True
+            dfs2(i, prev_dist+dist)
 
-dfs(1)
+# find max
+visit[1]=True
+dfs2(1, 0)
+second=-1
+maxi=-1
 
-print(ans)
+
+for idx, val in enumerate(distance1):
+    if maxi < val:
+        maxi = val
+        second=idx
+#print(second)
+#print(distance1)
+
+distance1=[0 for i in range(N+1)]
+visit=[False for i in range(N+1)]
+visit[second]=True
+dfs2(second, 0)
+
+maxi=-1
+#print(distance1)
+for idx, val in enumerate(distance1):
+    if maxi < val:
+        maxi = val
+
+if N == 2:
+    print(chart[1][0][1])
+else:
+    print(maxi)
