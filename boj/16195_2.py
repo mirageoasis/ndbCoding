31c2bf90a0f0a22b70841135609996957319d0e1
mirageoasis <kimhw0820@naver.com>
2024-12-13 13:56:23 +0900

    2023:8:22 완료

--- boj/16195_2.py
+++ boj/16195_2.py
@@ -1,6 +1,33 @@
+import sys
+
 t=int(input())
 
-
+div=1000000009
 
 for i in range(t):
-    pass
+    n, m = map(int, sys.stdin.readline().split())
+    dp=[[0 for i in range(m+1)] for j in range(n+1)]
+
+    dp[1][1]=1
+    if n >=2:
+        dp[2][1]=1
+        if m >=2:
+            dp[2][2]=1
+    
+    if n>=3:
+        dp[3][1]=1
+        if m >=2:
+            dp[3][2]=2
+        if m >=3:
+            dp[3][3]=1
+    
+    for i in range(4, n+1):
+        for j in range(2, m+1):
+            dp[i][j]=(dp[i-1][j-1] + dp[i-2][j-1] + dp[i-3][j-1]) % div
+
+    ans=0
+    for d in dp[n]:
+        ans = (ans+d) % div
+    print(ans)
+    # for d in dp:
+    #     print(d)
